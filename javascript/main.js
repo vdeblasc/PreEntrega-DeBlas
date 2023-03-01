a4d33c5ec2f541154404304f99e7dfe23e28a1b9
Valentino De Blas <tinodeblascaballer@gmail.com>
2023-03-01 12:33:07 -0300

    Tercer Entrega

--- javascript/main.js
+++ javascript/main.js
@@ -1,138 +1,721 @@
-
-
-// FUNCIÓN QUE CALCULA SI HAY QUE AGREGAR INTERESES A TU COMPRA
-
-function intereses(monto, medio) {
-    if (medio == "Tarjeta" || medio == "tarjeta") {
-        let precio_tarjeta = (monto * 0.15) + monto;
-        return precio_tarjeta;
-    }
-    else if (medio == "Transferencia" || medio == "transferencia") {
-        let precio_transferencia = monto;
-        return precio_transferencia;
-    }
-}
-
-
 // LISTA DE PRODUCTOS
 class Productos {
-    constructor(nombre, precio, stock) {
-        this.nombre = nombre;
+    constructor(id, categorias, distincion, imgSrc, precio, stock) {
+        this.id = id;
+        this.categorias = categorias;
+        this.distincion = distincion;
+        this.imgSrc = imgSrc;
         this.precio = precio;
-        this.stock = stock;
     }
 }
 
 let lista_productos = [];
 
-let nuevo_producto = new Productos("Remeras", 4500, 3);
-lista_productos.push(nuevo_producto);
-nuevo_producto = new Productos("Bermudas", 5000, 5);
-lista_productos.push(nuevo_producto);
-nuevo_producto = new Productos("Pantalones", 5500, 10);
-lista_productos.push(nuevo_producto);
-nuevo_producto = new Productos("Buzos", 7000, 2);
-lista_productos.push(nuevo_producto);
+lista_productos.push(
+    new Productos(
+        "M001",
+        "Niños Soquetes",
+        "Buzz Lightyear",
+        "./images/niño-buzz.jpg",
+        300
+    )
+);
 
-// VERIFICADOR DE MEDIO DE PAGO
+lista_productos.push(
+    new Productos(
+        "M002",
+        "Niños Soquetes",
+        "Corazones",
+        "./images/niño-corazon.jpg",
+        300
+    )
+);
 
-let medio = prompt("¿Quieres pagar por Transferencia o con Tarjeta?. Recordá que si pagas con tarjeta tiene un 15% de recargo.")
+lista_productos.push(
+    new Productos(
+        "M003",
+        "Niños Soquetes",
+        "Frozen-Olaf",
+        "./images/niño-frozen.jpg",
+        300
+    )
+);
+lista_productos.push(
+    new Productos(
+        "M004",
+        "Niños Soquetes",
+        "Iron Man",
+        "./images/niño-iron.jpg",
+        300
+    )
+);
 
-while (medio != "Tansferencia" && medio != "transferencia" && medio != "Tarjeta" && medio != "tarjeta") {
-    alert("Ingrese transferencia o tarjeta.");
-    medio = prompt(
-        "Ingresar si es transferencia o tarjeta. Recordá que si pagas con Tarjeta tenes un 15% de recargo.");
+lista_productos.push(
+    new Productos(
+        "M005",
+        "Niños Soquetes",
+        "Minions",
+        "./images/niño-minion.jpg",
+        300
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M006",
+        "Niños Caña",
+        "Stitch",
+        "./images/niño-stitch.jpg",
+        300
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M007",
+        "Niños Caña",
+        "Novia Stitch",
+        "./images/niño-stitch2.jpg",
+        300
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M008",
+        "Niños Soquetes",
+        "Novia Stitch",
+        "./images/niño-stitch3.jpg",
+        300
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M009",
+        "Niños Soquetes",
+        "Stitch",
+        "./images/niño-stitch4.jpg",
+        300
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M010",
+        "Caña",
+        "Alienigena",
+        "./images/caña-alienigena.jpg",
+        600
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M011",
+        "Caña",
+        "Blanco Arcoiris",
+        "./images/caña-blanco.jpg",
+        600
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M012",
+        "Caña",
+        "Negro Arcoiris",
+        "./images/caña-negro.jpg",
+        600
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M013",
+        "Caña",
+        "Smile BLack",
+        "./images/caña-smile.jpg",
+        600
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M014",
+        "Caña",
+        "Sonrisa",
+        "./images/caña-sonrisa.jpg",
+        600
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M015",
+        "Caña",
+        "Vans",
+        "./images/caña-vans.jpg",
+        600
+    )
+);
+
+
+lista_productos.push(
+    new Productos(
+        "M016",
+        "Caña",
+        "Triple Linea",
+        "./images/caña-verde.jpg",
+        600
+    )
+);
+
+
+lista_productos.push(
+    new Productos(
+        "M017",
+        "Caña Premium",
+        "Bilardo",
+        "./images/caña-premium-bilardo.jpg",
+        800
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M018",
+        "Caña Premium",
+        "Boca",
+        "./images/caña-premium-boca.jpg",
+        800
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M019",
+        "Caña Premium",
+        "Cars",
+        "./images/caña-premium-cars.jpg",
+        800
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M020",
+        "Caña Premium",
+        "Scooby Doo",
+        "./images/caña-premium-scooby-doo.jpg",
+        800
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M021",
+        "Caña Premium",
+        "Harry Potter",
+        "./images/caña-premium-harry.jpg",
+        800
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M022",
+        "Caña Premium",
+        "Monster Inc.",
+        "./images/caña-premium-monster.jpg",
+        800
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M023",
+        "Caña Premium",
+        "Igor",
+        "./images/caña-premium-winnie.jpg",
+        800
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M024",
+        "Caña Premium",
+        "Yoda",
+        "./images/caña-premium-yoda.jpg",
+        800
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M025",
+        "Soquetes",
+        "Buzz Lightyear",
+        "./images/soquete-buzz.jpg",
+        500
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M026",
+        "Soquetes",
+        "Coca-Cola",
+        "./images/soquete-coca.jpg",
+        500
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M027",
+        "Soquetes",
+        "Fanta",
+        "./images/soquete-fanta.jpg",
+        500
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M028",
+        "Soquetes",
+        "Mariposa",
+        "./images/soquete-mariposa.jpg",
+        500
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M029",
+        "Soquetes",
+        "Nubes",
+        "./images/soquete-nube.jpg",
+        500
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M030",
+        "Soquetes",
+        "Oreos",
+        "./images/soquete-oreo.jpg",
+        500
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M031",
+        "Soquetes",
+        "Simpsons",
+        "./images/soquete-simpsons.jpg",
+        500
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M032",
+        "Soquetes",
+        "Sonrisa",
+        "./images/soquete-sonrisa.jpg",
+        500
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M033",
+        "Soquetes",
+        "Spider-Man",
+        "./images/soquete-spider.jpg",
+        500
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M034",
+        "Soquetes",
+        "Star Wars",
+        "./images/soquete-star.jpg",
+        500
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M035",
+        "Soquetes",
+        "Star Wars-Coffee",
+        "./images/soquete-star2.jpg",
+        500
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M036",
+        "Soquetes",
+        "Star Wars-Dark Vader",
+        "./images/soquete-star3.jpg",
+        500
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M037",
+        "Soquetes",
+        "Stranger Things-Green",
+        "./images/soquete-stranger.jpg",
+        500
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M038",
+        "Soquetes",
+        "Stranger Things-White",
+        "./images/soquete-stranger2.jpg",
+        500
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M039",
+        "Soquetes Premium",
+        "Capitan America",
+        "./images/soquete-premium-america.jpg",
+        600
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M040",
+        "Soquetes Premium",
+        "Broccoli",
+        "./images/soquete-premium-broccoli.jpg",
+        600
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M041",
+        "Soquetes Premium",
+        "Friends",
+        "./images/soquete-premium-friends.jpg",
+        600
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M042",
+        "Soquetes Premium",
+        "Mickey Mouse",
+        "./images/soquete-premium-mickey.jpg",
+        600
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M043",
+        "Soquetes Premium",
+        "Thor",
+        "./images/soquete-premium-Thor.jpg",
+        600
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M044",
+        "Deportivas",
+        "Adidas-Pink",
+        "./images/deportiva1.jpg",
+        700
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M045",
+        "Deportivas",
+        "Adidas-Red",
+        "./images/deportiva2.jpg",
+        700
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M046",
+        "Deportivas",
+        "Adidas-Black and Red",
+        "./images/deportiva3.jpg",
+        700
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M047",
+        "Deportivas",
+        "Vans",
+        "./images/deportiva4.jpg",
+        700
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M048",
+        "Deportivas",
+        "Adidas-Black",
+        "./images/deportiva5.jpg",
+        700
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M049",
+        "Deportivas",
+        "Nike-Stars",
+        "./images/deportiva6.jpg",
+        700
+    )
+);
+
+lista_productos.push(
+    new Productos(
+        "M050",
+        "Deportivas",
+        "Puma",
+        "./images/deportiva7.jpg",
+        700
+    )
+);
+
+
+
+
+// RENDER PRODUCTOS
+
+let productos_agregar = document.querySelector(".productos");
+
+function renderizarProductos() {
+    lista_productos.forEach((producto) => {
+        productos_agregar.innerHTML += `
+            <div class='card'>
+                <img src='${producto.imgSrc}' class='card-img-top card_img' alt='imagen ${producto.nombre}'>
+                <div class='card_body'>
+                    <h4 class='categoria'>${producto.categorias}</h4>
+                    <h5 class='nombre'>${producto.distincion}</h5>
+                    <h5 class='card_precio'>${producto.precio}$</h5>
+                    <h6 id='card_id'>${producto.id}</h6>
+                    <button class='btn_agregar_carrito'>AGREGAR AL CARRITO</button>
+                </div>
+            </div>
+        `;
+    });
 }
 
-//CONTROL STOCK
+renderizarProductos();
 
-let compra = prompt("Escriba el nombre de lo que desea comprar: Remera $4.500, Bermuda $5.000, Pantalon $5.500 o Buzo $7.000. Si no desea comprar nada escriba Fin.");
 
-let cantidad_remeras = 0;
-let cantidad_bermudas = 0;
-let cantidad_pantalones = 0;
-let cantidad_buzos = 0;
-let precio_total_remeras = intereses(lista_productos[0].precio, medio);
-let precio_total_bermudas = intereses(lista_productos[1].precio, medio);
-let precio_total_pantalones = intereses(lista_productos[2].precio, medio);
-let precio_total_buzos = intereses(lista_productos[3].precio, medio);
 
-// BUCLE DE COMPRAS
 
-while (compra != "Fin" && compra != "fin") {
-    if (compra == "Remera" || compra == "remera" && lista_productos[0].stock > 0) {
-        cantidad_remeras++;
-        lista_productos[0].stock--;
+// CARRITO
+
+let btn_agregar = document.querySelectorAll(".btn_agregar_carrito");
+
+for (let btn of btn_agregar) {
+    btn.addEventListener("click", agregar_a_carrito);
+}
+
+// AGREGAR A CARRITO
+
+let carrito = JSON.parse(sessionStorage.getItem("carrito")) || [];
+
+function agregar_a_carrito(e) {
+    let button = e.target;
+    let idProducto = button.parentElement.querySelector("#card_id").textContent;
+    let categoriaProducto = button.parentElement.querySelector(
+        ".categoria"
+    ).textContent;
+    let distincionProducto =
+        button.parentElement.querySelector(".nombre").textContent;
+    let precioProducto =
+        button.parentElement.querySelector(".card_precio").textContent;
+    let imagenProducto =
+        button.parentElement.parentElement.querySelector("img").src;
+
+    let producto_carrito = {
+        idProducto: idProducto,
+        categoriaProducto: categoriaProducto,
+        distincionProducto: distincionProducto,
+        precio: precioProducto,
+        cantidad: 1,
+        img: imagenProducto,
+    };
+
+    aumentar_producto__carrito(producto_carrito);
+}
+
+// TOTAL CARRITO
+
+const carrito_total = () => {
+    const valor_total = document.getElementById("carrito_precio_total");
+
+    let total_a_pagar = 0;
+    carrito.forEach((producto) => {
+        const precio = parseInt(producto.precio);
+        const cantidad = parseInt(producto.cantidad);
+        total_a_pagar += precio * cantidad;
+
+
+    });
+    valor_total.innerHTML = `$${total_a_pagar}`;
+    sessionStorage.setItem("carrito", JSON.stringify(carrito));
+};
+
+// AGREGAR A CARRITO O AUMENTAR VALOR INPUT DESDE AGREGAR A CARRITO
+
+function aumentar_producto__carrito(producto_carrito) {
+    const inputProductoUnidades = document.getElementsByClassName("input_unidades");
+    for (let i = 0; i < carrito.length; i++) {
+        if (carrito[i].idProducto === producto_carrito.idProducto) {
+            carrito[i].cantidad++;
+            const inputValorNuevoUnidades = inputProductoUnidades[i];
+            inputValorNuevoUnidades.value++;
+            carrito_total();
+            return null;
+        }
     }
-    else if (compra == "Bermuda" || compra == "bermuda" && lista_productos[1].stock > 0) {
-        cantidad_bermudas++;
-        lista_productos[1].stock--;
+
+    carrito.push(producto_carrito);
+    render_carrito();
+}
+
+render_carrito();
+
+// RENDER CARRITO
+
+function render_carrito() {
+    let tabla = document.getElementById("carrito-items");
+    tabla.innerHTML = "";
+
+    for (let producto of carrito) {
+        let fila = document.createElement("tr");
+        fila.innerHTML = `<td><img src="${producto.img}" width="80px">
+                        <p class="id_producto">${producto.idProducto}</p></td>
+                        <td><p class="categoria_producto">${producto.categorias}</p></td>
+                        <td><p class="nombre_producto">${producto.distincion}</p></td>
+                        <td>
+                        <input type="number" min="1" class="input_unidades" value=${producto.cantidad}></td>
+                        <td class="prod_carrito_precio">${producto.precio}</td>
+                        <button class="btn btn-danger button btn_borrar_elemento">ELIMINAR</button>`;
+
+        tabla.append(fila);
+        carrito_total();
     }
-    else if (compra == "Pantalon" || compra == "pantalon" && lista_productos[2].stock > 0) {
-        cantidad_pantalones++;
-        lista_productos[2].stock--;
+
+    // AUMENTAR CLICKEANDO BOTON INPUT
+
+    const btn_input = document.querySelectorAll(".input_unidades");
+
+
+
+    for (let btn of btn_input) {
+        btn.addEventListener("change", sumaCantidad);
     }
-    else if (compra == "Buzo" || compra == "buzo" && lista_productos[3].stock > 0) {
-        cantidad_buzos++;
-        lista_productos[3].stock--;
+
+    function sumaCantidad(e) {
+
+        const sumaInput = e.target;
+        const idProd = sumaInput.parentElement.parentElement.querySelector(".id_producto").textContent;
+
+        carrito.forEach((item) => {
+            if (item.idProducto === idProd) {
+                sumaInput.value < 1 ? sumaInput.value = 1 : sumaInput.value;
+                item.cantidad = sumaInput.value;
+                carrito_total();
+            }
+        });
     }
+
+
+    // BORRAR ELEMENTOS
+
+    let btn_borrar_elemento = document.querySelectorAll(".btn_borrar_elemento");
+
+    for (let btn of btn_borrar_elemento) {
+        btn.addEventListener("click", eliminar_producto);
+    }
+
+    function eliminar_producto(e) {
+        let padre = e.target.parentNode;
+        let producto_eliminar = padre.querySelector(".id_producto").textContent;
+
+        for (let i = 0; i < carrito.length; i++) {
+            if (carrito[i].idProducto === producto_eliminar) {
+                carrito.splice(i, 1);
+                padre.remove();
+                carrito_total();
+            }
+        }
+    }
+
+    carrito_total();
+}
+
+
+let btn_pagar = document.querySelector('.btn-pagar');
+
+btn_pagar.addEventListener('click', () => {
+
+
+    let total_a_pagar = document.getElementById('carrito_precio_total').textContent.replace("$", "");
+
+    if (total_a_pagar > 0) {
+
+
+        document.body.innerHTML =
+            `<div class="gracias">
+            <h1 >¡GRACIAS POR SU COMPRA!</h1>
+            </div>
+            <div class="volver">
+            <a href="index.html" class= "volver_a_la_tienda">VOLVER A LA TIENDA</a>
+            </div>`}
     else {
-        alert("Ingresa un valor válido. Si ingreso un valor válido, puede ser que no tengamos stock");
+
+
+        document.body.innerHTML =
+            `<div class="ninguna">
+            <h1>¡NO HA REALIZADO NINGUNA COMPRA!</h1>
+            </div>
+            <div class="volver">
+            <a href="index.html" class= "volver_a_la_tienda">VOLVER A LA TIENDA</a>
+            </div>`
+
     }
 
-    compra = prompt(
-        "Desea agregar: Remera $4.500, Bermuda $5.000, Pantalon $5.500 o Buzo $7.000. Si no desea comprar nada escriba Fin."
-    );
-}
-
-// CÁLCULO TOTAL DE LAS COMPRAS
-
-if (cantidad_remeras > 0) {
-    alert(
-        "Compraste " +
-        cantidad_remeras +
-        " remera/s y gastaste $" +
-        cantidad_remeras * precio_total_remeras
-    );
-}
-if (cantidad_bermudas > 0) {
-    alert(
-        "Compraste " +
-        cantidad_bermudas +
-        " bermuda/s y gastaste $" +
-        cantidad_bermudas * precio_total_bermudas
-    );
-}
-if (cantidad_pantalones > 0) {
-    alert(
-        "Compraste " +
-        cantidad_pantalones +
-        " pantalon/es y gastaste $" +
-        cantidad_pantalones * precio_total_pantalones
-    );
-}
-if (cantidad_buzos > 0) {
-    alert(
-        "Compraste " +
-        cantidad_buzos +
-        " buzo/s y gastaste $" +
-        cantidad_buzos * precio_total_buzos
-    );
-}
-
-let cantidad_total =
-    cantidad_remeras * precio_total_remeras +
-    cantidad_bermudas * precio_total_bermudas + cantidad_pantalones * precio_total_pantalones +
-    cantidad_buzos * precio_total_buzos;
-
-if (cantidad_total != NaN) {
-    alert("Gastaste en total $" + cantidad_total);
-}
-
-//CONTROL INTERNO DE STOCK  
-
-let stock_falta = lista_productos.filter((producto) => producto.stock == 0);
-
-for (let producto of stock_falta) {
-    console.log("Hay que comprar stock de " + producto.nombre);
-}
+    ;
+    window.sessionStorage.clear()
+});
